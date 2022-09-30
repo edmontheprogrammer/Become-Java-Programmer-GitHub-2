500b2016058801bef75ba42262306187659b506f
Edmon the Programmer <edmontheprogrammer@gmail.com>
2022-09-30 10:33:54 +0800

    Become Django Developer Final Commit
    Become Django Developer, Django forms  Final Commit

---
+++ Learnining_Java/UseJARFileInAnotherProject/src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    public String name;
+
+    public Person(String myName) {
+        this.name = myName;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello " + name);
+    }
+}
---
+++ Learnining_Java/UseJARFileInAnotherProject2/src/Example.java
@@ -0,0 +1,8 @@
+public class Example {
+
+    public static void main(String[] args) {
+
+      //  Person person = Person("Bob");
+
+    }
+}
